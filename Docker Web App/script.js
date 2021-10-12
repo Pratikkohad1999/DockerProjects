d04f377052dee0e9eed2d0dd458d10dc7c5dcafd
Pratik Kohad <62031325+Pratikkohad1999@users.noreply.github.com>
2021-10-13 01:16:25 +0530

    Javascript file

---
+++ Docker Web App/script.js
@@ -0,0 +1,17 @@
+function lw()
+        {
+        
+        var xhr=new XMLHttpRequest();
+        i=document.getElementById("in1").value;
+        
+        xhr.open("GET","http://65.2.148.204/cgi-bin/lw.py?x="+i,true);
+        xhr.send();
+        
+        xhr.onload=function (){
+        var output=xhr.responseText;
+        document.getElementById("d1").innerHTML=output;
+                      }
+        }
+        
+        
+        
